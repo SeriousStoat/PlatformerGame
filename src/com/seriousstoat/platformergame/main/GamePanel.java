0372bc5441c80996668ab572b14021b65cef2b22
SeriousStoat <aseriousstoat@pm.me>
2024-07-23 10:23:54 -0700

    rectangle movement working

--- src/com/seriousstoat/platformergame/main/GamePanel.java
+++ src/com/seriousstoat/platformergame/main/GamePanel.java
@@ -9,8 +9,10 @@ import com.seriousstoat.platformergame.inputs.MouseInputs;
 public class GamePanel extends JPanel{
 
     private MouseInputs mouseInputs;
-    private int xDelta = 100, yDelta = 100;
-
+    private float xDelta = 50, yDelta = 100;
+    private float xDir = 0.03f, yDir = 0.01f;
+    private int frames = 0;
+    private long lastCheck = 0;
     public GamePanel() {
 
         mouseInputs = new MouseInputs(this);
@@ -21,23 +23,40 @@ public class GamePanel extends JPanel{
 
     public void changeXDelta(int value) {
         this.xDelta += value;
-        repaint();
     }
 
     public void changeYDelta(int value) {
         this.yDelta += value;
-        repaint();
     }
 
     public void setRectPos(int x, int y) {
         this.xDelta = x;
         this.yDelta = y;
-        repaint();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.fillRect(xDelta, yDelta, 200, 50);
+        updateRectangle();
+        g.fillRect((int) xDelta, (int) yDelta, 200, 50);
+
+        frames++;
+        if (System.currentTimeMillis() - lastCheck >= 1000) {
+            lastCheck = System.currentTimeMillis();
+            System.out.println("FPS: " + frames);
+            frames = 0;
+        }
+        
+        repaint();
+    }
+
+    private void updateRectangle() {
+        xDelta += xDir;
+        if (xDelta > 400 || xDelta < 0)
+            xDir *= -1;
+
+        yDelta += yDir;
+        if (yDelta > 400 || yDelta < 0)
+            yDir *= -1;
     }
 }
