9c93edd3b27ee7bad81260f92ab56c810ed3f77b
SeriousStoat <aseriousstoat@pm.me>
2024-07-23 10:40:00 -0700

    rectangle color change working

--- src/com/seriousstoat/platformergame/main/GamePanel.java
+++ src/com/seriousstoat/platformergame/main/GamePanel.java
@@ -1,6 +1,9 @@
 package com.seriousstoat.platformergame.main;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
+
 import javax.swing.JPanel;
 
 import com.seriousstoat.platformergame.inputs.KeyboardInputs;
@@ -13,8 +16,12 @@ public class GamePanel extends JPanel{
     private float xDir = 0.03f, yDir = 0.01f;
     private int frames = 0;
     private long lastCheck = 0;
+    private Color color = new Color(200, 20, 90);
+    private Random random;
+
     public GamePanel() {
 
+        random = new Random();
         mouseInputs = new MouseInputs(this);
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
@@ -38,6 +45,7 @@ public class GamePanel extends JPanel{
         super.paintComponent(g);
 
         updateRectangle();
+        g.setColor(color);
         g.fillRect((int) xDelta, (int) yDelta, 200, 50);
 
         frames++;
@@ -52,11 +60,25 @@ public class GamePanel extends JPanel{
 
     private void updateRectangle() {
         xDelta += xDir;
-        if (xDelta > 400 || xDelta < 0)
+        if (xDelta > 400 || xDelta < 0) {
             xDir *= -1;
+            color = getRndColor();
+        }
+            
 
         yDelta += yDir;
-        if (yDelta > 400 || yDelta < 0)
+        if (yDelta > 400 || yDelta < 0) {
             yDir *= -1;
+            color = getRndColor();
+        }
+            
+    }
+
+    private Color getRndColor() {
+        int r = random.nextInt(255);
+        int g = random.nextInt(255);
+        int b = random.nextInt(255);
+
+        return new Color(r, g, b);
     }
 }
