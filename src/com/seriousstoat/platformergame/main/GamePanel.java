d5cd129618bc4d2da95f21f7e4e59aaee61289cd
SeriousStoat <aseriousstoat@pm.me>
2024-07-23 17:45:21 -0700

    removed test code

--- src/com/seriousstoat/platformergame/main/GamePanel.java
+++ src/com/seriousstoat/platformergame/main/GamePanel.java
@@ -1,8 +1,6 @@
 package com.seriousstoat.platformergame.main;
 
-import java.awt.Color;
 import java.awt.Graphics;
-import java.util.Random;
 
 import javax.swing.JPanel;
 
@@ -14,12 +12,9 @@ public class GamePanel extends JPanel{
     private MouseInputs mouseInputs;
     private float xDelta = 50, yDelta = 100;
     private float xDir = 3, yDir = 1;
-    private Color color = new Color(200, 20, 90);
-    private Random random;
 
     public GamePanel() {
 
-        random = new Random();
         mouseInputs = new MouseInputs(this);
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
@@ -42,35 +37,8 @@ public class GamePanel extends JPanel{
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        updateRectangle();
-        g.setColor(color);
-        g.fillRect((int) xDelta, (int) yDelta, 200, 50);
-
         
         
     }
 
-    private void updateRectangle() {
-        xDelta += xDir;
-        if (xDelta > 400 || xDelta < 0) {
-            xDir *= -1;
-            color = getRndColor();
-        }
-            
-
-        yDelta += yDir;
-        if (yDelta > 400 || yDelta < 0) {
-            yDir *= -1;
-            color = getRndColor();
-        }
-            
-    }
-
-    private Color getRndColor() {
-        int r = random.nextInt(255);
-        int g = random.nextInt(255);
-        int b = random.nextInt(255);
-
-        return new Color(r, g, b);
-    }
 }
