b262cb552674c76e629c5c73effd82f7e11499fb
SeriousStoat <aseriousstoat@pm.me>
2024-07-25 11:20:48 -0700

    UPS created and synced with FPS to catch lost frames

--- src/com/seriousstoat/platformergame/main/Game.java
+++ src/com/seriousstoat/platformergame/main/Game.java
@@ -6,6 +6,7 @@ public class Game implements Runnable {
     private GamePanel gamePanel;
     private Thread gameThread;
     private final int FPS_SET = 120;
+    private final int UPS_SET = 200;
 
     public Game() {
         gamePanel = new GamePanel();
@@ -24,13 +25,30 @@ public class Game implements Runnable {
     public void run() {
         
         double timePerFrame = 1000000000.0 / FPS_SET;
+        double timePerUpdate = 1000000000.0 / UPS_SET;
         long lastFrame = System.nanoTime();
-        int frames = 0;
-        long lastCheck = System.currentTimeMillis();
         long now = System.nanoTime();
+
+        long previousTime = System.nanoTime();
+
+        int frames = 0;
+        int updates = 0;
+        long lastCheck = System.currentTimeMillis();
+        double deltaU = 0;
 
         while (true) {
             now = System.nanoTime();
+            long currentTime = System.nanoTime();
+
+            deltaU += (currentTime - previousTime) / timePerUpdate;
+            previousTime = currentTime;
+
+            if (deltaU >= 1) {
+                // update();
+                updates++;
+                deltaU--;
+            }
+
             if (now - lastFrame >= timePerFrame) {
                 gamePanel.repaint();
                 lastFrame = now;
@@ -38,8 +56,9 @@ public class Game implements Runnable {
             }
             if (System.currentTimeMillis() - lastCheck >= 1000) {
             lastCheck = System.currentTimeMillis();
-            System.out.println("FPS: " + frames);
+            System.out.println("FPS: " + frames + " | UPS: " + updates);
             frames = 0;
+            updates = 0;
         }
         }
     }
