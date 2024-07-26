a3d4a9413c60e33ce01fc7150a7b123a586b49f9
SeriousStoat <aseriousstoat@pm.me>
2024-07-26 10:56:39 -0700

    Added gravity & juming variables

--- src/com/seriousstoat/platformergame/entities/Player.java
+++ src/com/seriousstoat/platformergame/entities/Player.java
@@ -18,11 +18,18 @@ public class Player extends Entity {
     private int aniTick, aniIndex, aniSpeed = 15;
     private int playerAction = IDLE;
     private boolean moving = false, attacking = false;
-    private boolean up, left, down, right;
+    private boolean up, left, down, right, jump;
     private float playerSpeed = 2.0f;
     private int[][] lvlData;
     private float xDrawOffset = 21 * Game.SCALE;
     private float yDrawOffset = 4 * Game.SCALE;
+
+    // Jumping & Gravity
+    private float airSpeed = 0f;
+    private float gravity = 0.04f * Game.SCALE;
+    private float jumpSpeed = -2.25f * Game.SCALE;
+    private float fallSpeedAfterCollision = 0.5f * Game.SCALE;
+    private boolean inAir = false;
 
     public Player(float x, float y, int width, int height) {
         super(x, y, width, height);
