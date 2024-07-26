587b6a1b9a2c4f49d832093bd460039a835f52d5
SeriousStoat <aseriousstoat@pm.me>
2024-07-26 10:06:56 -0700

    player position now controlled by hitbox

--- src/com/seriousstoat/platformergame/entities/Player.java
+++ src/com/seriousstoat/platformergame/entities/Player.java
@@ -9,6 +9,7 @@ import static com.seriousstoat.platformergame.utilz.HelpMethods.CanMoveHere;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
+import com.seriousstoat.platformergame.main.Game;
 import com.seriousstoat.platformergame.utilz.LoadSave;
 
 public class Player extends Entity {
@@ -20,16 +21,18 @@ public class Player extends Entity {
     private boolean up, left, down, right;
     private float playerSpeed = 2.0f;
     private int[][] lvlData;
+    private float xDrawOffset = 21 * Game.SCALE;
+    private float yDrawOffset = 4 * Game.SCALE;
 
     public Player(float x, float y, int width, int height) {
         super(x, y, width, height);
         loadAnimations();
+        initHitbox(x, y, 20 * Game.SCALE, 28 * Game.SCALE);
     }
     
     public void update() {
 
         updatePos();
-        updateHitbox();
         updateAnimationTick();
         setAnimation();
         
@@ -37,7 +40,7 @@ public class Player extends Entity {
 
     public void render(Graphics g) {
 
-        g.drawImage(animations[playerAction][aniIndex], (int) x, (int) y, width, height, null);
+        g.drawImage(animations[playerAction][aniIndex], (int) (hitbox.x - xDrawOffset), (int) (hitbox.y - yDrawOffset), width, height, null);
         drawHitbox(g);
     }
 
@@ -94,11 +97,17 @@ public class Player extends Entity {
         else if (down && !up) 
             ySpeed = playerSpeed;
 
-        if (CanMoveHere(x+xSpeed, y+ySpeed, width, height, lvlData)) {
-            this.x += xSpeed;
-            this.y += ySpeed;
+        // if (CanMoveHere(x+xSpeed, y+ySpeed, width, height, lvlData)) {
+        //     this.x += xSpeed;
+        //     this.y += ySpeed;
+        //     moving = true; 
+        // }
+
+        if (CanMoveHere(hitbox.x+xSpeed, hitbox.y+ySpeed, hitbox.width, hitbox.height, lvlData)) {
+            hitbox.x += xSpeed;
+            hitbox.y += ySpeed;
             moving = true; 
-        }
+    }
 
 
     }
