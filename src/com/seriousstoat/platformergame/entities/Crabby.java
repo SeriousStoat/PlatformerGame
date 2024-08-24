ef131392ed7684e05a724d10281baa2fb3d1d5b1
SeriousStoat <aseriousstoat@pm.me>
2024-08-24 09:31:20 -0700

    Added subclass Crabby to Entity.Enemy

---
+++ src/com/seriousstoat/platformergame/entities/Crabby.java
@@ -0,0 +1,12 @@
+package com.seriousstoat.platformergame.entities;
+
+import static com.seriousstoat.platformergame.utilz.Constants.Enemy.*;;
+
+public class Crabby extends Enemy {
+
+    public Crabby(float x, float y, int width, int height, int enemyType) {
+        super(x, y, CRABBY_WIDTH, CRABBY_HEIGHT, CRABBY);
+        
+    }
+
+}
