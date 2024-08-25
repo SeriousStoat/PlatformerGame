553b5d7064307dd0c777bddffe18aeaff08ecae4
SeriousStoat <aseriousstoat@pm.me>
2024-08-24 19:59:03 -0700

    Moved some Enemy methods to Crabby class, to allow differences in enemy types

--- src/com/seriousstoat/platformergame/entities/Crabby.java
+++ src/com/seriousstoat/platformergame/entities/Crabby.java
@@ -1,8 +1,10 @@
 package com.seriousstoat.platformergame.entities;
 
 import static com.seriousstoat.platformergame.utilz.Constants.Enemy.*;
+import static com.seriousstoat.platformergame.utilz.HelpMethods.*;
+import static com.seriousstoat.platformergame.utilz.Constants.Directions.*;
 
-import com.seriousstoat.platformergame.main.Game;;
+import com.seriousstoat.platformergame.main.Game;
 
 public class Crabby extends Enemy {
 
@@ -11,4 +13,28 @@ public class Crabby extends Enemy {
         initHitbox(x, y, (int) (22 * Game.SCALE), (int) (19 * Game.SCALE));
     }
 
+    public void update(int[][] lvlData) {
+        updateMove(lvlData);
+        updateAnimationTick();
+        
+    }
+
+    private void updateMove(int[][] lvlData) {
+        if (firstUpdate)
+            firstUpdateCheck(lvlData);
+         
+        if (inAir)
+            updateInAir(lvlData);
+        else {
+            switch(enemyState) {
+                case IDLE:
+                    newState(RUNNING);
+                    break;
+                case RUNNING:
+                    move(lvlData);
+
+                    break;
+            }
+        }
+    }
 }
--- src/com/seriousstoat/platformergame/entities/Enemy.java
+++ src/com/seriousstoat/platformergame/entities/Enemy.java
@@ -1,20 +1,21 @@
 package com.seriousstoat.platformergame.entities;
 
 import static com.seriousstoat.platformergame.utilz.Constants.Enemy.*;
-import static com.seriousstoat.platformergame.utilz.HelpMethods.*;
 import static com.seriousstoat.platformergame.utilz.Constants.Directions.*;
+import static com.seriousstoat.platformergame.utilz.HelpMethods.*;
+
 
 import com.seriousstoat.platformergame.main.Game;
 
 public abstract class Enemy extends Entity {
-    private int aniIndex, enemyState, enemyType;
-    private int aniTick, aniSpeed = 25;
-    private boolean firstUpdate = true;
-    private boolean inAir;
-    private float fallSpeed;
-    private float gravity = 0.04F * Game.SCALE;
-    private float walkSpeed = 0.35F * Game.SCALE;
-    private int walkDir = LEFT;
+    protected int aniIndex, enemyState, enemyType;
+    protected int aniTick, aniSpeed = 25;
+    protected boolean firstUpdate = true;
+    protected boolean inAir;
+    protected float fallSpeed;
+    protected float gravity = 0.04F * Game.SCALE;
+    protected float walkSpeed = 0.35F * Game.SCALE;
+    protected int walkDir = LEFT;
     
     public Enemy(float x, float y, int width, int height, int enemyType) {
         super(x, y, width, height);
@@ -23,7 +24,46 @@ public abstract class Enemy extends Entity {
         
     }
 
-    private void updateAnimationTick() {
+    protected void firstUpdateCheck(int[][] lvlData) {
+        if (!IsEntityOnFloor(hitbox, lvlData))
+            inAir = true;
+        firstUpdate = false;
+    }
+
+    protected void updateInAir(int[][] lvlData) {
+        if (CanMoveHere(hitbox.x, hitbox.y + fallSpeed, hitbox.width, hitbox.height, lvlData)) {
+            hitbox.y += fallSpeed;
+            fallSpeed += gravity;
+        } else {
+            inAir = false;
+            hitbox.y = GetEntityYPosUnderRoofOrAboveFloor(hitbox, fallSpeed);
+        }
+    }
+
+    protected void move(int[][] lvlData) {
+        float xSpeed = 0;
+
+        if (walkDir == LEFT)
+            xSpeed = -walkSpeed;
+        else
+            xSpeed = walkSpeed;
+
+        if (CanMoveHere(hitbox.x + xSpeed, hitbox.y, hitbox.width, hitbox.height, lvlData))
+            if(IsFloor(hitbox, xSpeed, lvlData)) {
+                hitbox.x += xSpeed;
+                return;
+            }
+        
+        changeWalkDir();
+    }
+
+    protected void newState(int enemyState) {
+        this.enemyState = enemyState;
+        aniTick = 0;
+        aniIndex = 0;
+    }
+
+    protected void updateAnimationTick() {
         aniTick++;
         if (aniTick >= aniSpeed) {
             aniTick = 0;
@@ -34,54 +74,7 @@ public abstract class Enemy extends Entity {
         }
     }
 
-    public void update(int[][] lvlData) {
-        updateMove(lvlData);
-        updateAnimationTick();
-        
-    }
-
-    private void updateMove(int[][] lvlData) {
-        if (firstUpdate) {
-            if (!IsEntityOnFloor(hitbox, lvlData))
-                inAir = true;
-            firstUpdate = false;
-        }
-         
-        if (inAir) {
-            if (CanMoveHere(hitbox.x, hitbox.y + fallSpeed, hitbox.width, hitbox.height, lvlData)) {
-                hitbox.y += fallSpeed;
-                fallSpeed += gravity;
-            } else {
-                inAir = false;
-                hitbox.y = GetEntityYPosUnderRoofOrAboveFloor(hitbox, fallSpeed);
-            }
-        } else {
-            switch(enemyState) {
-                case IDLE:
-                    enemyState = RUNNING;
-                    break;
-                case RUNNING:
-                    float xSpeed = 0;
-
-                    if (walkDir == LEFT)
-                        xSpeed = -walkSpeed;
-                    else
-                        xSpeed = walkSpeed;
-
-                    if (CanMoveHere(hitbox.x + xSpeed, hitbox.y, hitbox.width, hitbox.height, lvlData))
-                        if(IsFloor(hitbox, xSpeed, lvlData)) {
-                            hitbox.x += xSpeed;
-                            return;
-                        }
-                    
-                    changeWalkDir();
-
-                    break;
-            }
-        }
-    }
-
-    private void changeWalkDir() {
+    protected void changeWalkDir() {
         if (walkDir == LEFT)
             walkDir = RIGHT;
         else
