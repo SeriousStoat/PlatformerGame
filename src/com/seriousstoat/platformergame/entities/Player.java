080e130555664c40d2df4a3713b5141db86c93ea
SeriousStoat <aseriousstoat@pm.me>
2024-09-04 20:30:32 -0700

    hiding hitboxes

--- src/com/seriousstoat/platformergame/entities/Player.java
+++ src/com/seriousstoat/platformergame/entities/Player.java
@@ -110,7 +110,7 @@ public class Player extends Entity {
                 (int) (hitbox.y - yDrawOffset),
                 width * flipW, height, null);
         // drawHitbox(g, lvlOffset);
-        drawAttackBox(g, lvlOffset);
+        // drawAttackBox(g, lvlOffset);
         drawUI(g);
     }
 
